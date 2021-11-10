949a134b5e50e013cbab5293b3517790f3266e98
Frank-Proulx <frankproulx7@gmail.com>
2021-11-10 13:04:02 -0800

    add monster class having passed test
    Co-Authored-by: Albert Lee <leesga8@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,16 +1,36 @@
 import { exportAllDeclaration } from "@babel/types";
 import { testPathPatternToRegExp } from "jest-util";
 import {Character} from './../src/js/character.js'
+import {Monster} from './../src/js/character.js'
+
 
 
 describe('Character', () => {
 
-  test('should correctly create a character class with a name and 4 attributes', () => {
+  test('should correctly create a character class with a name and 4 attributes, an xp attribute that starts at 0 and a level that starts at 1', () => {
     const character1 = new Character("Bob", 4, 5, 3, 3);
     expect(character1.name).toEqual("Bob");
     expect(character1.health).toEqual(4);
     expect(character1.strength).toEqual(5);
     expect(character1.wisdom).toEqual(3);
     expect(character1.charm).toEqual(3);
+    expect(character1.xp).toEqual(0);
+    expect(character1.level).toEqual(1);
+  });
+
+  test('should correctly reassign xp and level values', () => {
+    const character1 = new Character("Bob", 4, 5, 3, 3);
+    character1.xp = 120;
+    character1.addLevel();
+    expect(character1.xp).toEqual(10);
+    expect(character1.level).toEqual(2);
+  });
+});
+
+describe('Monster', () => {
+
+  test('it should create a monster class object with level, HP, attack', () => {
+    const monster1 = new Monster();
+    expect().toEqual();
   });
 });
--- src/js/character.js
+++ src/js/character.js
@@ -5,5 +5,29 @@ export class Character {
     this.strength = strength;
     this.wisdom = wisdom;
     this.charm = charm;
+    this.xp = 0;
+    this.level = 1;
+    this.hitPoints = health * 50;
+  }
+
+  addLevel() {
+    let xpNeeded = 100 + (this.level * 10); 
+    if (this.xp >= xpNeeded) {
+      this.xp = this.xp - xpNeeded;
+      this.level++;
+    }
+  }
+
+  doBattle() {
+  
+  }
+}
+
+export class Monster extends Character {
+  constructor(){
+    super();
+    this.level = Math.floor(Math.random()*19 + 1);
+    this.hitPoints = 20 * this.level;
+    this.attack = 20 * this.level;
   }
 }
